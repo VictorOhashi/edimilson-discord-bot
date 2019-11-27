4fa5e05b9d0d323edc84083f849ded473154999d
Victor Ohashi <victor.ohashi@softplan.com.br>
2019-11-27 17:51:41 -0300

    Changed prefix

--- index.js
+++ index.js
@@ -1,90 +1,90 @@
-require('dotenv').config()
-const Discord = require('discord.js')
-const client = new Discord.Client()
+require("dotenv").config();
+const Discord = require("discord.js");
+const client = new Discord.Client();
 
-let pessoas = ['2570']
-let prefix = '!'
+let pessoas = ["2570"];
+let prefix = ".";
 
 const frases = [
-  'Foda....',
-  'Não é o ideal mas acontece',
-  'Sla só sei que o André é gay',
-  'Comi sua mãe',
-  'Sua irmã é minha',
-  'Corno!!',
-  'Já queimou seu Judeu hj??',
-  'Já fez sua parte? Deu paulada em maconheiro hj?',
+  "Foda....",
+  "Não é o ideal mas acontece",
+  "Sla só sei que o André é gay",
+  "Comi sua mãe",
+  "Sua irmã é minha",
+  "Corno!!",
+  "Já queimou seu Judeu hj??",
+  "Já fez sua parte? Deu paulada em maconheiro hj?",
   `Sim eu sou LGBT:
    Levanta
    Gay
    Bora
    Trabalhar`,
-  'Suicídio é sempre uma opção',
-  'Desistir é pros fracos, faça como eu nem tente',
-  'Talkei',
-  'Imposto é roubo',
-  'Bandindo tem q morre Talkei',
-  'Meus filhos são mto bem educados',
-  'Não roube! O Governo não gosta de concorrência.',
-  'Minha autoestima está lá no céu. Morta.',
-  'Não olhei pra baixo... pisei no email',
-  'Não sou gay apenas um hétero diferenciado',
-  'Não esqueça de respirar',
-  'Existem mais aviões no fundo do mar do que submarinos voando',
-  'A luta é grande, mas a derrota é certa',
-  'Mano, ela é diferente das outras',
-  '5 centímetros de puro prazer, 3 segundos de pura emoção',
-  'Que porra de musica é essa que ta tocando caralho',
-  'Pare de tentar e comece a desistir'
-]
+  "Suicídio é sempre uma opção",
+  "Desistir é pros fracos, faça como eu nem tente",
+  "Talkei",
+  "Imposto é roubo",
+  "Bandindo tem q morre Talkei",
+  "Meus filhos são mto bem educados",
+  "Não roube! O Governo não gosta de concorrência.",
+  "Minha autoestima está lá no céu. Morta.",
+  "Não olhei pra baixo... pisei no email",
+  "Não sou gay apenas um hétero diferenciado",
+  "Não esqueça de respirar",
+  "Existem mais aviões no fundo do mar do que submarinos voando",
+  "A luta é grande, mas a derrota é certa",
+  "Mano, ela é diferente das outras",
+  "5 centímetros de puro prazer, 3 segundos de pura emoção",
+  "Que porra de musica é essa que ta tocando caralho",
+  "Pare de tentar e comece a desistir"
+];
 
 const andre = [
-  'Vocé é muito gay',
-  'Dps te apresento um instrumento mágico... cortador de unha',
-  'Para de ficar desviando o olhar',
-  'Volta pra roça de onde tu veio',
-  'Olha pra mim se quiser falar comigo',
-  'Inazuma é um lixo',
-  'Mega Men nem é jogo',
-  'Não dirijo a palavra a negrinhos',
-  'Vagabundo...',
-  'Mano vai jogar seus emulador lá'
-]
+  "Vocé é muito gay",
+  "Dps te apresento um instrumento mágico... cortador de unha",
+  "Para de ficar desviando o olhar",
+  "Volta pra roça de onde tu veio",
+  "Olha pra mim se quiser falar comigo",
+  "Inazuma é um lixo",
+  "Mega Men nem é jogo",
+  "Não dirijo a palavra a negrinhos",
+  "Vagabundo...",
+  "Mano vai jogar seus emulador lá"
+];
 
 const messages = [
   {
-    pergunta: ['bom dia', 'buenos dias', 'good morning'],
+    pergunta: ["bom dia", "buenos dias", "good morning"],
     resposta:
       'Mermão bom dia é o caralho parcero, isso aqui é o grupo da torcida jovem, entendeu? Tu quer dar bom dia tu cria um grupo de viado, de GLS, e fica "bom dia", "boa tarde", "boa noite", ou então tu cria um grupo pra tua família, aí tu fica dando bom dia. Aqui é psicopata, ladrão, bandido, cheirador, vendedor de droga, polícia maluco, polícia assaltante, aqui tem a porra toda mermão, isso aqui é a Torcida Jovem do Flamengo! Bom dia é o caralho, rapá! Toma no cu...﻿'
   },
   {
-    pergunta: ['onde eu deixo esse saco de batata'],
+    pergunta: ["onde eu deixo esse saco de batata"],
     resposta:
-      'Deixa ai mano!!! Sai dai mano!!! Sai daqui!!! Sai com essas batata ai meu'
+      "Deixa ai mano!!! Sai dai mano!!! Sai daqui!!! Sai com essas batata ai meu"
   },
   {
-    pergunta: ['quem e voce', 'quem é voce', 'quem é vc'],
-    resposta: 'Eu sou Edmilson o quebra galho do restaurante!'
+    pergunta: ["quem e voce", "quem é voce", "quem é vc"],
+    resposta: "Eu sou Edmilson o quebra galho do restaurante!"
   },
   {
-    pergunta: ['koe', 'eae'],
+    pergunta: ["koe", "eae"],
     resposta: () => frases[getRandomInt(0, frases.length - 1)]
   }
-]
+];
 
 function getRandomInt(min, max) {
-  min = Math.ceil(min)
-  max = Math.floor(max)
-  return Math.floor(Math.random() * (max - min + 1)) + min
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function getRandomColor() {
-  var letters = '0123456789ABCDEF'
-  var color = '#'
+  var letters = "0123456789ABCDEF";
+  var color = "#";
   for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)]
+    color += letters[Math.floor(Math.random() * 16)];
   }
-  return color
+  return color;
 }
 
 const createRole = async (msg, arg, isRandom) => {
@@ -92,9 +92,9 @@ const createRole = async (msg, arg, isRandom) => {
     try {
       await msg.guild.roles
         .find(role => role.name === msg.author.discriminator)
-        .delete()
+        .delete();
     } catch (e) {
-      console.error(e)
+      console.error(e);
     }
   }
   await msg.member.guild
@@ -103,81 +103,81 @@ const createRole = async (msg, arg, isRandom) => {
       color: isRandom ? arg : `#${arg}`,
       position: 18
     })
-    .catch(console.error)
+    .catch(console.error);
 
   msg.member.addRole(
     msg.guild.roles.find(r => r.name === msg.author.discriminator)
-  )
-  msg.channel.send(`${msg.author} sua cor agora é #${arg}!`)
-}
+  );
+  msg.channel.send(`${msg.author} sua cor agora é #${arg}!`);
+};
 
-client.on('message', msg => {
-  if (!msg.content.startsWith(prefix) || msg.author.bot) return
-  const args = msg.content.slice(prefix.length).split(' ')
-  const command = args.shift().toLowerCase()
+client.on("message", msg => {
+  if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+  const args = msg.content.slice(prefix.length).split(" ");
+  const command = args.shift().toLowerCase();
 
-  const hasPermission = msg.member.roles.find(r => r.name === 'Guarda Real')
+  const hasPermission = msg.member.roles.find(r => r.name === "Guarda Real");
 
   if (hasPermission) {
-    if (command === 'setcor') {
-      const arg = args[0]
+    if (command === "setcor") {
+      const arg = args[0];
       if (arg && arg.length === 6) {
-        createRole(msg, arg, false)
+        createRole(msg, arg, false);
       } else if (!arg) {
-        msg.channel.send(`Gerando cor aleatória para ${msg.author}!`)
-        createRole(msg, getRandomColor(), true)
+        msg.channel.send(`Gerando cor aleatória para ${msg.author}!`);
+        createRole(msg, getRandomColor(), true);
       } else {
-        msg.channel.send(`So aceito valor do tipo hexcolor, ${msg.author}!`)
+        msg.channel.send(`So aceito valor do tipo hexcolor, ${msg.author}!`);
       }
     }
   } else {
-    msg.channel.send(`${msg.author} você é fraco, lhe falta odio!`)
+    msg.channel.send(`${msg.author} você é fraco, lhe falta odio!`);
   }
-})
+});
 
-client.on('message', msg => {
-  if (msg.author.discriminator === '5733') {
-    if (!msg.content.startsWith(prefix) || msg.author.bot) return
-    const args = msg.content.slice(prefix.length).split(' ')
-    const command = args.shift().toLowerCase()
-    if (command === 'setpessoa') {
+client.on("message", msg => {
+  if (msg.author.discriminator === "5733") {
+    if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+    const args = msg.content.slice(prefix.length).split(" ");
+    const command = args.shift().toLowerCase();
+    if (command === "setpessoa") {
       if (!args.length) {
-        msg.channel.send(`Digita o comando direito pó, ${msg.author}!`)
+        msg.channel.send(`Digita o comando direito pó, ${msg.author}!`);
       } else {
-        pessoas.push(args.shift())
-        msg.channel.send(`Esse maluco foi adicionado na lista ${args}!`)
+        pessoas.push(args.shift());
+        msg.channel.send(`Esse maluco foi adicionado na lista ${args}!`);
       }
-    } else if (command === 'listpessoas') {
-      msg.channel.send(`Esses caras estão na lista, ${pessoas}!`)
-    } else if (command === 'clearpessoas') {
-      pessoas = ['2570']
-      msg.channel.send(`Relexa limpei a lista!`)
-    } else if (command === 'ajuda') {
+    } else if (command === "listpessoas") {
+      msg.channel.send(`Esses caras estão na lista, ${pessoas}!`);
+    } else if (command === "clearpessoas") {
+      pessoas = ["2570"];
+      msg.channel.send(`Relexa limpei a lista!`);
+    } else if (command === "ajuda") {
       msg.channel.send(
-        'Pode usar esses comandos ai: setPessoa, listPessoas, clearPessoas, setCor'
-      )
+        "Pode usar esses comandos ai: setPessoa, listPessoas, clearPessoas, setCor"
+      );
     }
   }
-})
+});
 
-client.on('message', msg => {
+client.on("message", msg => {
   messages.forEach(m => {
-    const pergunta = m.pergunta.find(p => msg.content.toLowerCase() === p)
+    const pergunta = m.pergunta.find(p => msg.content.toLowerCase() === p);
     if (pergunta)
       msg.channel.send(
-        typeof m.resposta === 'function' ? m.resposta() : m.resposta
-      )
-  })
-})
+        typeof m.resposta === "function" ? m.resposta() : m.resposta
+      );
+  });
+});
 
-client.on('message', msg => {
-  if (msg.content.startsWith(prefix) || msg.author.bot) return
-  const pessoa = pessoas.find(p => msg.author.discriminator === p)
-  if (pessoa === '2570') {
-    msg.reply(andre[getRandomInt(0, andre.length - 1)])
+client.on("message", msg => {
+  if (msg.content.startsWith(prefix) || msg.author.bot) return;
+  const pessoa = pessoas.find(p => msg.author.discriminator === p);
+  if (pessoa === "2570") {
+    msg.reply(andre[getRandomInt(0, andre.length - 1)]);
   } else if (pessoa) {
-    msg.reply(frases[getRandomInt(0, frases.length - 1)])
+    msg.reply(frases[getRandomInt(0, frases.length - 1)]);
   }
-})
+});
 
-client.login(process.env.BOT_TOKEN)
+client.login(process.env.BOT_TOKEN);
